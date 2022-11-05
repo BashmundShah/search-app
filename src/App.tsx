ae3f1a162f71a938e0f4f9b18268f17973358548
Bashmund Shah <bashmundkhan@yahoo.com>
2022-11-05 23:44:38 +0100

    design update

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,47 @@
 import React from "react";
 import { useGetRecordsByNameQueryQuery } from "./services/odmb";
+import { ReactComponent as Search } from "./assets/search.svg";
+import { ReactComponent as VideoLibrary } from "./assets/video_library.svg";
+import { Record } from "./services/types";
 
+const recordJSX = (record: Record) => (
+  <div className="h-24 flex rounded bg-gray-100 cursor-pointer hover:bg-gray-200 hover:border border-solid border-indigo-500 transition-all hover:shadow-md hover:scale-110 duration-200 ease-in-out">
+    <img className="rounded-l" src={record.Poster} />
+    <p className="w-full px-2 font-semibold text-center place-self-center">
+      {record.Title}
+    </p>
+  </div>
+);
 function App() {
-  const catchPhrase = "Unlimited movies, TV shows, and more. Watch anywhere.";
+  const catchPhrase = "Unlimited Movies, TV Shows, and More.";
   const [searchString, setSearchString] = React.useState("");
   const { data, error, isLoading } =
     useGetRecordsByNameQueryQuery(searchString);
-  React.useEffect(() => {
-    console.log("data:", data);
-    console.log("error: ", error);
-    console.log("isLoading: ", isLoading);
-  }, [data]);
+
+  const records = data?.Search;
+  const isFetched = data?.Response === "True";
   return (
-    <div className="w-full h-screen flex flex-col items-center bg-slate-500">
-      <nav className="py-2 w-full bg-green-500 sm:bg-yellow-500 md:bg-orange-500 lg:bg-red-500 text-center">
-        Search App
+    <div className="w-full h-screen flex flex-col items-center bg-gray-300">
+      <nav className="py-2 text-white flex justify-center w-full bg-indigo-500 sm:bg-yellow-500 md:bg-orange-500 lg:bg-red-500">
+        <VideoLibrary className=" fill-current" />
+        <span className="ml-2 tracking-widest font-sans">NeetFlix</span>
       </nav>
-      <div className="bg-slate-300 w-full h-full p-8 flex flex-col items-center">
-        <input
-          className="mb-8"
-          onChange={(e) => setSearchString(e.target.value)}
-        ></input>
-        <h1 className="w-full text-center">{catchPhrase}</h1>
+      <div className=" mt-4 w-full text-center flex flex-col flex-grow overflow-hidden space-y-4 px-4">
+        <h1>{catchPhrase}</h1>
+        <div className="flex items-center p-2 bg-white rounded text-black text-sm">
+          <input
+            className="bg-transparent w-full outline-none"
+            placeholder="Type to search..."
+            onChange={(e) => setSearchString(e.target.value)}
+          />
+          <Search className="w-5 h-5 stroke-current" />
+        </div>
+        <div className="w-full">
+          <h2>Search Results</h2>
+          <div className="space-y-4 h-1/3 overflow-y-auto no-scrollbar p-4">
+            {isFetched && records?.map((record) => recordJSX(record))}
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/services/types.ts
+++ src/services/types.ts
@@ -1,7 +1,14 @@
-export type Records = {
+export type Record = {
   Poster: string;
   Title: string;
   Type: string;
   Year: string;
   imdbID: string;
+};
+
+export type Records = {
+  Response: string;
+  Error: string;
+  Search: Record[];
+  totalResults: string;
 };
