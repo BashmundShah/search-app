30014b9123784924f992007dc7c63caedf8ef70a
Bashmund Shah <bashmundkhan@yahoo.com>
2022-11-06 12:33:15 +0100

    made design responsive

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,23 @@ import React from "react";
 import { useGetRecordsByNameQueryQuery } from "./services/odmb";
 import { ReactComponent as Search } from "./assets/search.svg";
 import { ReactComponent as VideoLibrary } from "./assets/video_library.svg";
+import { ReactComponent as ImagePlaceholder } from "./assets/image_not_supported.svg";
 import { Record } from "./services/types";
 
-const recordJSX = (record: Record) => (
-  <div className="h-24 flex rounded bg-gray-100 cursor-pointer hover:bg-gray-200 hover:border border-solid border-indigo-500 transition-all hover:shadow-md hover:scale-110 duration-200 ease-in-out">
-    <img className="rounded-l" src={record.Poster} />
+const recordJSX = (record: Record, key: number) => (
+  <div
+    key={key}
+    className="h-24 w-full max-w-sm mx-auto flex rounded bg-gray-100 cursor-pointer hover:bg-gray-200 hover:border border-solid border-indigo-500 transition-all hover:shadow-2xl hover:-translate-y-1 duration-200 ease-in-out"
+  >
+    {record.Poster === "N/A" ? (
+      <div className="w-24 flex items-center justify-center bg-gray-300 rounded m-1">
+        <ImagePlaceholder />
+      </div>
+    ) : (
+      <div className="flex items-center w-20 h-24">
+        <img className="rounded-l object-cover" src={record.Poster} />
+      </div>
+    )}
     <p className="w-full px-2 font-semibold text-center place-self-center">
       {record.Title}
     </p>
@@ -15,33 +27,40 @@ const recordJSX = (record: Record) => (
 function App() {
   const catchPhrase = "Unlimited Movies, TV Shows, and More.";
   const [searchString, setSearchString] = React.useState("");
-  const { data, error, isLoading } =
-    useGetRecordsByNameQueryQuery(searchString);
+  const { data, isLoading } = useGetRecordsByNameQueryQuery(searchString);
 
   const records = data?.Search;
-  const isFetched = data?.Response === "True";
+  const isDataFetched = data?.Response === "True";
   return (
     <div className="w-full h-screen flex flex-col items-center bg-gray-300">
-      <nav className="py-2 text-white flex justify-center w-full bg-indigo-500 sm:bg-yellow-500 md:bg-orange-500 lg:bg-red-500">
+      <nav className="py-2 text-white flex justify-center w-full bg-indigo-500">
         <VideoLibrary className=" fill-current" />
         <span className="ml-2 tracking-widest font-sans">NeetFlix</span>
       </nav>
-      <div className=" mt-4 w-full text-center flex flex-col flex-grow overflow-hidden space-y-4 px-4">
+      <div className="py-4 w-full text-center flex flex-col items-center flex-grow overflow-hidden space-y-4 px-4">
         <h1>{catchPhrase}</h1>
-        <div className="flex items-center p-2 bg-white rounded text-black text-sm">
+        <div className="flex w-full max-w-md items-center p-2 bg-white rounded text-black text-sm group focus-within:ring-1 ring-solid ring-indigo-700">
           <input
             className="bg-transparent w-full outline-none"
             placeholder="Type to search..."
             onChange={(e) => setSearchString(e.target.value)}
           />
-          <Search className="w-5 h-5 stroke-current" />
+          <Search className="w-5 h-5 stroke-current group-focus-within:text-indigo-700" />
         </div>
-        <div className="w-full">
-          <h2>Search Results</h2>
-          <div className="space-y-4 h-1/3 overflow-y-auto no-scrollbar p-4">
-            {isFetched && records?.map((record) => recordJSX(record))}
+        {searchString !== "" && (
+          <div className="w-full overflow-hidden pb-8">
+            <h2>Search Results</h2>
+            <div className="w-full">
+              {isDataFetched ? (
+                <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 h-full overflow-y-scroll p-4">
+                  {records?.map((record, index) => recordJSX(record, index))}
+                </div>
+              ) : (
+                <h2 className="text-red-500">{data?.Error}</h2>
+              )}
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
